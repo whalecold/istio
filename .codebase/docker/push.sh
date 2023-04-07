30c7506041f07b5b50a4cb18967002875453905c
夏岩 <xiayan.cn@bytedance.com>
2023-04-07 09:04:14 +0000

    feat(ci): add ci
    * add ci files
    * add more
    * skip codebase
    * fx name
    * miss -o
    * add coverage.out
    
    https://code.byted.org/infcp/istio/merge_requests/18

---
+++ .codebase/docker/push.sh
@@ -0,0 +1,2 @@
+docker build . -t hub.byted.org/istio-testing/build-tools:release-1.16-fb2454846dcbf0275675ce7d7e22e2602ff1aa38-with-envoy
+docker push hub.byted.org/istio-testing/build-tools:release-1.16-fb2454846dcbf0275675ce7d7e22e2602ff1aa38-with-envoy
