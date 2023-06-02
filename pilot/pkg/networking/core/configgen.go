c10541229b4f644435b5d318211a09a376c6bc86
zhenglisheng <zhenglisheng.zhengls@bytedance.com>
2023-06-02 10:40:48 +0800

    add file

--- pilot/pkg/networking/core/configgen.go
+++ pilot/pkg/networking/core/configgen.go
@@ -43,6 +43,9 @@ type ConfigGenerator interface {
 	// BuildHTTPRoutes returns the list of HTTP routes for the given proxy. This is the RDS output
 	BuildHTTPRoutes(node *model.Proxy, req *model.PushRequest, routeNames []string) ([]*discovery.Resource, model.XdsLogDetails)
 
+	// BuildVirtualHosts returns the list of VirtualHost for the given proxy HTTP routes. This is the VHDS output
+	BuildVirtualHosts(node *model.Proxy, req *model.PushRequest, virtualHostNames []string) ([]*discovery.Resource, model.XdsLogDetails)
+
 	// BuildNameTable returns list of hostnames and the associated IPs
 	BuildNameTable(node *model.Proxy, push *model.PushContext) *dnsProto.NameTable
 
---
+++ pilot/pkg/xds/vhds.go
@@ -0,0 +1,33 @@
+package xds
+
+import (
+	"fmt"
+
+	"istio.io/istio/pilot/pkg/model"
+)
+
+// VhdsGenerator implements the new Generate method for VHDS, using the in-memory, optimized endpoint storage in DiscoveryServer.
+type VhdsGenerator struct {
+	Server *DiscoveryServer
+}
+
+var _ model.XdsResourceGenerator = &VhdsGenerator{}
+
+func (vhds *VhdsGenerator) Generate(proxy *model.Proxy, w *model.WatchedResource, req *model.PushRequest) (model.Resources, model.XdsLogDetails, error) {
+	return nil, model.DefaultXdsLogDetails, fmt.Errorf("vhds unsupport sotw ads")
+}
+
+func (vhds *VhdsGenerator) GenerateDeltas(proxy *model.Proxy, req *model.PushRequest,
+	w *model.WatchedResource,
+) (model.Resources, model.DeletedResources, model.XdsLogDetails, bool, error) {
+	if !vhdsNeedsPush(req) {
+		return nil, nil, model.DefaultXdsLogDetails, true, nil
+	}
+	resources, logDetails := vhds.Server.ConfigGenerator.BuildHTTPRoutes(proxy, req, w.ResourceNames)
+	return resources, nil, logDetails, true, nil
+}
+
+func vhdsNeedsPush(req *model.PushRequest) bool {
+	// TODO
+	return false
+}
